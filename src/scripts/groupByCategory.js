e19275e7e6f692d777abb4cf7d8ff068a522c68d
Volodymyr Bilohorodskyi <niko.fm@gmail.com>
2024-10-12 08:29:35 +0300

    homework done

--- src/scripts/groupByCategory.js
+++ src/scripts/groupByCategory.js
@@ -1,23 +1,42 @@
-import { PATH_DB } from "../constants/path.js";
+import {PATH_DB} from "../constants/path.js";
 import fs from "node:fs/promises";
 
 const groupByCategory = async () => {
-  const products = await fs.readFile(PATH_DB, "UTF-8");
-  const parsedProducts = JSON.parse(products);
-  const categories = parsedProducts.map((product) => product.category);
-  //   const filteredCategories = categories.filter((category, index, array) => {
-  //     return array.indexOf(category) === index;
-  //   });
-  const result = {};
-  categories.forEach((category) => {
-    result[category] = [];
-    parsedProducts.forEach((product) => {
-      if (category === product.category) {
-        result[category].push(product);
-      }
-    });
-  });
-  console.log(result);
+    const products = await fs.readFile(PATH_DB, "UTF-8");
+    const parsedProducts = JSON.parse(products);
+    const categories = parsedProducts.map(product => product.category);
+    //   const filteredCategories = categories.filter((category, index, array) => {
+    //     return array.indexOf(category) === index;
+    //   });
+    // на уроці
+    // const result = {};
+    //   categories.forEach((category) => {
+    //     result[category] = [];
+    //     parsedProducts.forEach((product) => {
+    //       if (category === product.category) {
+    //         result[category].push(product);
+    //       }
+    //     });
+    //   });
+    // кінець на уроці
+    // опрацювали самостійно
+    // const result = {};
+    // parsedProducts.forEach(product => {
+    //     if (!result[product.category]) {
+    //         result[product.category] = [];
+    //     }
+    //     result[product.category].push(product);
+    // });
+    // console.log(result);
+    // кінець опрацювали самостійно
+    // чат жипити
+    const result = parsedProducts.reduce((acc, product) => {
+        (acc[product.category] = acc[product.category] || []).push(product);
+        return acc;
+    }, {});
+
+    console.log(result);
+    // кінець чат жипити
 };
 
 groupByCategory();
